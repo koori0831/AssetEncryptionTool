74d033cbfb80106082711572c957a0204a883270
koori0831 <fuyunokoori0831@gmail.com>
2025-02-16 23:29:44 +0900

    Add: Utilities

---
+++ Utilities.cs
@@ -0,0 +1,53 @@
+﻿using System.Windows.Forms;
+
+namespace AssetEncryptionTool2
+{
+    public static class Utilities
+    {
+        public static string GetFilePathFromDialog(string title, string filter)
+        {
+            using (OpenFileDialog openFileDialog = new OpenFileDialog())
+            {
+                openFileDialog.Title = title;
+                openFileDialog.Filter = filter;
+                openFileDialog.RestoreDirectory = true;
+
+                if (openFileDialog.ShowDialog() == DialogResult.OK)
+                {
+                    return openFileDialog.FileName;
+                }
+                return null;
+            }
+        }
+
+        public static string GetSaveFilePathFromDialog(string title, string filter)
+        {
+            using (SaveFileDialog saveFileDialog = new SaveFileDialog())
+            {
+                saveFileDialog.Title = title;
+                saveFileDialog.Filter = filter;
+                saveFileDialog.RestoreDirectory = true;
+
+                if (saveFileDialog.ShowDialog() == DialogResult.OK)
+                {
+                    return saveFileDialog.FileName;
+                }
+                return null;
+            }
+        }
+
+        public static string GetFolderPathFromDialog(string title = "폴더 선택")
+        {
+            using (FolderBrowserDialog folderBrowserDialog = new FolderBrowserDialog())
+            {
+                folderBrowserDialog.Description = title;
+                folderBrowserDialog.ShowNewFolderButton = true;
+                if (folderBrowserDialog.ShowDialog() == DialogResult.OK)
+                {
+                    return folderBrowserDialog.SelectedPath;
+                }
+                return null;
+            }
+        }
+    }
+}
